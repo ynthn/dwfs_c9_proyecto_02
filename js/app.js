4fc1496034917904bec5e4f3931a8dda29dd126b
yonathan muñoz braavo <yonathan@MacBook-Air-de-yonathan.local>
2023-08-08 22:09:56 -0400

    estructura visual

---
+++ js/app.js
@@ -0,0 +1,31 @@
+
+
+
+/**
+ * EVENT ADD TASK
+ */
+let formSubmit = document.getElementById("form");
+formSubmit.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let task = document.getElementById("task");
+
+    console.log(task);
+
+});
+
+
+/**
+ * ADD TASK LOCALSTORAGE
+ */
+function addTask(task,detail){
+
+}
+
+/**
+ * CLEAN FORM
+ */
+function cleanForm(){
+    
+}
+
