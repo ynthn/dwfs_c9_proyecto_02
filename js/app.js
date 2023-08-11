a21da567dd175382d045ffd02b09f5ff883554cb
yonathan muñoz braavo <yonathan@MacBook-Air-de-yonathan.local>
2023-08-10 22:39:07 -0400

    funciones de agregar y leer datos

--- js/app.js
+++ js/app.js
@@ -8,10 +8,12 @@ let formSubmit = document.getElementById("form");
 formSubmit.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    let task = document.getElementById("task");
+    let task = document.getElementById("task").value;
+    let detail = document.getElementById("detail").value;
 
-    console.log(task);
-
+    addTask(task, detail);
+    cleanForm();
+    showData();
 });
 
 
@@ -19,17 +21,54 @@ formSubmit.addEventListener("submit", function (e) {
  * ADD TASK LOCALSTORAGE
  */
 function addTask(task, detail) {
-   
+    let listData;
+    if (window.localStorage.getItem("listData") == null) {
+        listData = [];
+    } else {
+        listData = JSON.parse(window.localStorage.getItem("listData"));
+    }
+    listData.push({
+        task: task,
+        detail: detail
+    });
+    window.localStorage.setItem("listData", JSON.stringify(listData));
 }
 
 /**
  * CLEAN FORM
  */
 function cleanForm() {
-    let task = document.getElementById("task");
-    task.value = "";
-
-    let detail = document.getElementById("detail");
-    detail.value = "";
+    document.getElementById("task").value = "";
+    document.getElementById("detail").value = "";
 }
 
+
+/**
+ * SHOW DATA IN TABLE
+ */
+function showData() {
+    let listData;
+    if (window.localStorage.getItem("listData") == null) {
+        listData = [];
+    } else {
+        listData = JSON.parse(window.localStorage.getItem("listData"));
+    }
+    let html = "";
+    let count = 0;
+    listData.forEach(element => {
+        count++;
+        html += "<tr>";
+        html += "<td>" + count + "</td>";
+        html += "<td>" + element.task + "</td>";
+        html += "<td>" + element.detail + "</td>";
+        html += "<td class='text-center'>";
+        html += "<buttom class='btn btn-danger'>BORRAR</buttom>";
+        html += "<buttom class='btn btn-primary'>EDITAR</buttom>";
+        html += "</td>";
+        html += "</tr>";
+    });
+    document.querySelector("#table-data tbody").innerHTML = html;
+}
+
+
+showData();
