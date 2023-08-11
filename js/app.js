cc57da1ce9f1c93c5be6770c2413ee52efcb26d1
yonathan muñoz braavo <yonathan@MacBook-Air-de-yonathan.local>
2023-08-10 23:28:58 -0400

    funcionalidad de eliminar datos

--- js/app.js
+++ js/app.js
@@ -41,6 +41,62 @@ function cleanForm() {
     document.getElementById("task").value = "";
     document.getElementById("detail").value = "";
 }
+//const modalUpdate = new bootstrap.Modal(document.getElementById('modal-delete'));
+
+
+const modalDelete = new bootstrap.Modal(document.getElementById('modal-delete'));
+/**
+ * SHOW MODAL DELETE DATA
+ * @param {*} index 
+ */
+function showModalDelete(index) {
+    document.getElementById("delete-index").value = index;
+
+    let listData;
+    if (window.localStorage.getItem("listData") == null) {
+        listData = [];
+    } else {
+        listData = JSON.parse(window.localStorage.getItem("listData"));
+    }
+
+    document.getElementById("delete-task").value = listData[index].task;
+    document.getElementById("delete-detail").value = listData[index].detail;
+
+    modalDelete.show();
+}
+
+/**
+ * HIDE MODAL DELETE DATA
+ */
+function hideModalDelete() {
+    document.getElementById("delete-index").value = "";
+
+    modalDelete.hide();
+}
+
+
+/**
+ * DELETE DATA
+ */
+let formDelete = document.getElementById("formDelete");
+formDelete.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let index = document.getElementById("delete-index").value;
+
+    let listData;
+    if (window.localStorage.getItem("listData") == null) {
+        listData = [];
+    } else {
+        listData = JSON.parse(window.localStorage.getItem("listData"));
+    }
+
+    listData.splice(index, 1);
+    window.localStorage.setItem("listData", JSON.stringify(listData));
+
+    showData();
+    hideModalDelete();
+});
 
 
 /**
@@ -55,15 +111,15 @@ function showData() {
     }
     let html = "";
     let count = 0;
-    listData.forEach(element => {
+    listData.forEach(function (element, index) {
         count++;
         html += "<tr>";
         html += "<td>" + count + "</td>";
         html += "<td>" + element.task + "</td>";
         html += "<td>" + element.detail + "</td>";
         html += "<td class='text-center'>";
-        html += "<buttom class='btn btn-danger'>BORRAR</buttom>";
-        html += "<buttom class='btn btn-primary'>EDITAR</buttom>";
+        html += "<buttom class='btn btn-danger' onClick='showModalDelete(" + index + ")'>BORRAR</buttom>";
+        html += "<buttom class='btn btn-primary' onClick='showModalUpdate(" + index + ")'>EDITAR</buttom>";
         html += "</td>";
         html += "</tr>";
     });
